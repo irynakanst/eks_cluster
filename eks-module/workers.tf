92b2e165c2a609ebaf1af9ee59e5f21fe3ea26e9
irynakanst <iryna28660@gmail.com>
2024-05-09 22:10:46 -0500

    Modifying variables in workers.tf

--- eks-module/workers.tf
+++ eks-module/workers.tf
@@ -62,9 +62,7 @@ resource "aws_eks_node_group" "eks_worker_node" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.cluster-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.cluster-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.cluster-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_worker_attach_policy
   ]
 }
 
