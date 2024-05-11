f6bc4d5b95f04cbb0d7fab9461d002d5ddca15a3
irynakanst <iryna28660@gmail.com>
2024-05-10 20:38:58 -0500

    Starting EKS Cluster

--- eks-module/outputs.tf
+++ eks-module/outputs.tf
@@ -1,10 +1,15 @@
-data "aws_security_group" "eks_worker_sg_id" {
-  filter {
-    name   = "description"
-    values = ["EKS created security group applied to ENI that is attached to EKS Control Plane master nodes, as well as any managed workloads."]
-  }
-}
+# data "aws_security_group" "eks_worker_sg_id" {
+#   filter {
+#     name   = "description"
+#     values = ["EKS created security group applied to ENI that is attached to EKS Control Plane master nodes, as well as any managed workloads."]
+#   }
+# }
+
+# output "eks_worker_sg_id" {
+#   value       = data.aws_security_group.eks_worker_sg_id.id
+# }
+
 
 output "eks_worker_sg_id" {
-  value       = data.aws_security_group.eks_worker_sg_id.id
+  value       = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
 }
