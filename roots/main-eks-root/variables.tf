3145802d3e2363bfd0489c140a299fd63b8a9c90
irynakanst <iryna28660@gmail.com>
2024-06-24 19:08:48 -0500

    Commented rds variables.tf

--- roots/main-eks-root/variables.tf
+++ roots/main-eks-root/variables.tf
@@ -182,101 +182,101 @@ variable "eks_worker_max_unavailable" {
 
 ### RDS INSTANCE 
 
-variable "allocated_storage" {
-  type    = number
-  default = 10
-}
-
-variable "db_name" {
-  type = string
-}
-
-variable "db_engine" {
-  type = string
-}
-
-variable "db_engine_version" {
-  type = string
-}
-
-variable "inst_class" {
-  type = string
-}
-
-variable "db_username" {
-  type = string
-}
-
-variable "db_backup_retention_period" {
-  type    = number
-  default = 7
-}
-
-variable "db_identifier" {
-  type = string
-}
-
-variable "db_managed_password" {
-  type    = string
-  default = "true"
-}
-
-variable "db_tags" {
-  type = string
-}
-
-### DB SUBNET GROUP
-
-variable "db_subnet_gr_name" {
-  type = string
-}
-
-
-variable "db_subnet_gr_tags" {
-  type = string
-}
-
-### DB SECURITY GROUP
-
-variable "db_sg_name" {
-  type = string
-}
-
-variable "db_sg_description" {
-  type = string
-}
-
-variable "db_sg_tags" {
-  type = string
-}
-
-variable "db_ingress_port" {
-  type = number
-}
-
-variable "db_ingress_protocol" {
-  type    = string
-  default = "tcp"
-}
-
-# variable "eks_worker_sg_id" {
-#   type        = list
+# variable "allocated_storage" {
+#   type    = number
+#   default = 10
 # }
 
-variable "db_egress_port" {
-  type    = number
-  default = 0
-}
+# variable "db_name" {
+#   type = string
+# }
 
-variable "db_egress_protocol" {
-  type    = string
-  default = "-1"
-}
+# variable "db_engine" {
+#   type = string
+# }
 
-variable "db_egress_cidr" {
-  type    = list(any)
-  default = ["0.0.0.0/0"]
-}
+# variable "db_engine_version" {
+#   type = string
+# }
+
+# variable "inst_class" {
+#   type = string
+# }
+
+# variable "db_username" {
+#   type = string
+# }
+
+# variable "db_backup_retention_period" {
+#   type    = number
+#   default = 7
+# }
+
+# variable "db_identifier" {
+#   type = string
+# }
+
+# variable "db_managed_password" {
+#   type    = string
+#   default = "true"
+# }
+
+# variable "db_tags" {
+#   type = string
+# }
+
+# ### DB SUBNET GROUP
+
+# variable "db_subnet_gr_name" {
+#   type = string
+# }
+
+
+# variable "db_subnet_gr_tags" {
+#   type = string
+# }
+
+# ### DB SECURITY GROUP
+
+# variable "db_sg_name" {
+#   type = string
+# }
+
+# variable "db_sg_description" {
+#   type = string
+# }
+
+# variable "db_sg_tags" {
+#   type = string
+# }
+
+# variable "db_ingress_port" {
+#   type = number
+# }
+
+# variable "db_ingress_protocol" {
+#   type    = string
+#   default = "tcp"
+# }
+
+# # variable "eks_worker_sg_id" {
+# #   type        = list
+# # }
+
+# variable "db_egress_port" {
+#   type    = number
+#   default = 0
+# }
+
+# variable "db_egress_protocol" {
+#   type    = string
+#   default = "-1"
+# }
+
+# variable "db_egress_cidr" {
+#   type    = list(any)
+#   default = ["0.0.0.0/0"]
+# }
 
 ### WORKER NODE LAUNCH TEMPLATE
 
