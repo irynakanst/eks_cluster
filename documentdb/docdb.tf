e33ae6ea30b372a88b141182159d93a5e06c2bb6
irynakanst <iryna28660@gmail.com>
2024-06-24 22:23:25 -0500

    Fixed egress rule

--- documentdb/docdb.tf
+++ documentdb/docdb.tf
@@ -75,7 +75,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_eks_worker_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.docdb-sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 0
+#   from_port         = 0
+#   to_port           = 0
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
