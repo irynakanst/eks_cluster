90bb21d10810ec942b4fdc513793ce34d087a316
irynakanst <iryna28660@gmail.com>
2024-06-25 17:22:18 -0500

    Added KSM key

---
+++ documentdb/kms_key.tf
@@ -0,0 +1,55 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "docdb-kms-key" {
+  description             = "A docdb-test symmetric encryption KMS key"
+  deletion_window_in_days = 7
+  key_usage = "ENCRYPT_DECRYPT"
+  enable_key_rotation  = false
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/Iryna"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow use of the key"
+        Effect = "Allow"
+        Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/gitHubActionsIAMroleDevTerraform"
+        },
+        Action = [
+            "kms:DescribeKey",
+            "kms:Encrypt",
+            "kms:Decrypt",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey",
+            "kms:GenerateDataKeyWithoutPlaintext"
+        ],
+        Resource = "*"
+       },
+       {
+        Sid    = "Allow use of the key"
+        Effect = "Allow"
+        Principal = {
+            AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GitHubActionsCICDrole"
+        },
+        Action = [
+            "kms:DescribeKey",
+            "kms:Encrypt",
+            "kms:Decrypt",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey",
+            "kms:GenerateDataKeyWithoutPlaintext"
+        ],
+        Resource = "*"
+        }
+      ]
+  })
+}
--- documentdb/secret.tf
+++ documentdb/secret.tf
@@ -1,13 +1,18 @@
+### Creating random password
+
 resource "random_password" "docdb_password" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
+### Creating secret
 
 resource "aws_secretsmanager_secret" "docdb_secret" {
   name = "docdb-secret"
   recovery_window_in_days = 0
 }
+
+### Adding secret values
 
 resource "aws_secretsmanager_secret_version" "docdb_values" {
   secret_id     = aws_secretsmanager_secret.docdb_secret.id
@@ -17,9 +22,13 @@ resource "aws_secretsmanager_secret_version" "docdb_values" {
   })
 }
 
+### Retriving the secret
+
 data "aws_secretsmanager_secret" "docdb_secret" {
   name = aws_secretsmanager_secret.docdb_secret.name
 }
+
+### Retriving the secret values
 
 data "aws_secretsmanager_secret_version" "docdb_values" {
   secret_id = data.aws_secretsmanager_secret.docdb_secret.id
